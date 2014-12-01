b8811590a3193d2ff47d8c29057e31b39c7c5b62
Nathan Lintz <nlintz@gmail.com>
2014-12-01 02:07:59 -0500

    added images

--- test/test_swt.py
+++ test/test_swt.py
@@ -5,26 +5,23 @@ import numpy as np
 from matplotlib import pyplot as plt
 import cv2
 import math
-
+from profiler import *
 
 def test_imageSWT():
   img = cv2.imread('sofsign.jpg',0)
   B,G,R = cv2.split(cv2.imread('sofsign.jpg',1))
   img_color = cv2.merge((R,G,B))
-
   swt_pos = swt.strokeWidthTransform(img, 1)
-  swt_neg = swt.strokeWidthTransform(img, -1)
+  # swt_neg = swt.strokeWidthTransform(img, -1)
 
   plt.subplot(2,2,1)
   plt.imshow(img_color, interpolation="none")
   plt.subplot(2,2,2)
   plt.imshow(swt_pos, cmap="gray", interpolation="none")
-  plt.subplot(2,2,3)
-  plt.imshow(swt_neg, cmap="gray", interpolation="none")
-  plt.subplot(2,2,4)
-  plt.imshow(255-cv2.dilate(255-swt_neg, kernel = np.ones((2,2),np.uint8), iterations = 1), cmap="gray", interpolation="none")
-
-
+  # plt.subplot(2,2,3)
+  # plt.imshow(swt_neg, cmap="gray", interpolation="none")
+  # plt.subplot(2,2,4)
+  # plt.imshow(255-cv2.dilate(255-swt_neg, kernel = np.ones((2,2),np.uint8), iterations = 1), cmap="gray", interpolation="none")
   plt.show()
 
 def test_gradient():
