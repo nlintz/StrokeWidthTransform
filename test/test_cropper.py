0d7aa37d480c6aa40c483489a4c0feedfb4bb3bd
Nathan Lintz <nlintz@gmail.com>
2014-12-17 01:04:41 -0500

    added padding parameter

--- test/test_cropper.py
+++ test/test_cropper.py
@@ -1,4 +1,6 @@
 import sys, os
+import os.path
+print sys.path[0]
 sys.path.append(os.path.join(os.path.dirname(__file__), '../', 'lib'))
 import lib.textCropper as tc
 import numpy as np
@@ -10,7 +12,7 @@ import itertools
 
 def test_text_cropper():
   img = cv2.imread('test/images/fallout_shelter.jpg')
-  croppedRegions = tc.TextCropper.cropTextRegionsFromImage(img)
+  croppedRegions = tc.TextCropper.cropTextRegionsFromImage(img, 20)
   for i in range(len(croppedRegions)):
     plt.subplot(len(croppedRegions), 1, i+1)
     b,g,r = cv2.split(croppedRegions[i])
