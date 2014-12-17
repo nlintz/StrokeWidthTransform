17b01b61c7acdab8720f7306d094de806cc38217
dianavermilya <dvermilya@onshape.com>
2014-12-17 01:05:21 -0500

    added find_text function

--- test/test_tessaract.py
+++ test/test_tessaract.py
@@ -23,10 +23,8 @@ img = cv2.imread('images/fallout_6.jpg',0)
 clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
 cl1 = clahe.apply(img)
 cv2.imwrite('images/fallout_6_better.jpg', cl1)
-
 cv2_im = cv2.imread('images/fallout_6_better.jpg',0)
 pil_im = Image.fromarray(cv2_im)
-
 print pytesseract.image_to_string(pil_im, lang="eng")
 
 
