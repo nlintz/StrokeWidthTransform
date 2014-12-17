595243aac070412b7bec8c49fe9c43a86b155452
dianavermilya <dvermilya@onshape.com>
2014-12-17 14:08:09 -0500

    tesseract implemented

--- test/test_cropper.py
+++ test/test_cropper.py
@@ -8,16 +8,35 @@ import cv2
 import math, random
 from lib.profiler import *
 import itertools
+import Image
+import pytesseract
+
+
+def find_text(img):
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+  clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+  cl1 = clahe.apply(gray)
+  cv2.imwrite('placeholder.jpg', cl1)
+  cv2_im = cv2.imread('placeholder.jpg',0)
+  pil_im = Image.fromarray(cv2_im)
+  return pytesseract.image_to_string(pil_im, lang="eng")
 
 def test_text_cropper():
-  img = cv2.imread('images/caution_laser.jpg')
-  croppedRegions = tc.TextCropper.cropTextRegionsFromImage(img)
+  if len(sys.argv) > 1:
+    image_name= sys.argv[1]
+  else:
+    image_name = "emergency_stop"
+  img = cv2.imread('images/' + image_name + '.jpg')
+  croppedRegions = tc.TextCropper.cropTextRegionsFromImage(img, 20)
   for i in range(len(croppedRegions)):
     plt.subplot(len(croppedRegions), 1, i+1)
-    b,g,r = cv2.split(croppedRegions[i])
-    rgbImg = cv2.merge([r,g,b])
-    # cv2.imwrite('fallout_'+str(i)+'.jpg', croppedRegions[i])
-    plt.imshow(rgbImg)
+    if croppedRegions[i].size != 0:
+      b,g,r = cv2.split(croppedRegions[i])
+      rgbImg = cv2.merge([r,g,b])
+      # cv2.imwrite('fallout_'+str(i)+'.jpg', croppedRegions[i])
+      print find_text(rgbImg)
+      plt.imshow(rgbImg)
   plt.show()
 
 if __name__ == "__main__":
